c736b15bf000c5ad7242e1022a799e4d79b0a39b
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2023-07-06 13:17:24 +0200

    Add files via upload

---
+++ tryitout/tryItOutpg96.js
@@ -0,0 +1,14 @@
+var Myname=prompt("What is your name?")
+
+if(Myname==="Sheldon")
+{
+    alert("Hello Me");
+}else if(Myname==="Cobus")
+{
+    alert("Hello Dad")
+}else if(Myname==="Janine")
+{
+    alert("Hello Mom")
+}else{
+    alert("Howdy Stranger")
+};
---
+++ tryitout/tryItoutThree.js
@@ -0,0 +1,9 @@
+for(let i=3;i<10000; i*=3){
+    console.log(i);
+};
+
+x=3;
+while(x<10000){
+    console.log(x);
+    x*=3;
+}
