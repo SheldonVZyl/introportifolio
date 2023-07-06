899fadf301a999cd220d7d6ab1fc465849e567b1
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2023-07-06 12:34:28 +0200

    Add files via upload

---
+++ chapter11/chapter11.js
@@ -0,0 +1,6 @@
+var people = ["Kareem", "Fatma", "Sumaya", "Mr. Hobson", "Ms. Young", "Mrs. O' Doherty"];
+var otherPeople = ["Emilie", "Mommy", "Dad", "Kathy"];
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+
+people = p
