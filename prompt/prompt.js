2aca5c7cd3463a0a67c55f9946e8d3f5ecdd1098
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2023-07-06 13:13:34 +0200

    Add files via upload

---
+++ prompt/prompt.js
@@ -0,0 +1,9 @@
+/*var nam= prompt("What's your name?");
+console.log("Hello " + nam);*/
+
+var likesCats = confirm("Do you like cats?");
+if (likesCats) {
+    alert("You're a cool cat!");
+} else {
+    alert("Yeah, that's fine. You're still cool!");
+}
