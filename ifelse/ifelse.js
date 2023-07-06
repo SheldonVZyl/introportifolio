06fb047e81708fb8c4aeb2cccd31ecb4695df6e8
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2023-07-06 13:08:41 +0200

    Add files via upload

---
+++ ifelse/ifelse.js
@@ -0,0 +1,12 @@
+var lemonChicken = false;
+var beefWithBlackBean = false;
+var sweetAndSourPork = true;
+if (lemonChicken) {
+ console.log("Great! I'm having lemon chicken!");
+} else if (beefWithBlackBean) {
+ console.log("I'm having the beef.");
+} else if (sweetAndSourPork) {
+ console.log("OK, I'll have the pork.");
+} else {
+ console.log("Well, I guess I'll have rice then.");
+}
